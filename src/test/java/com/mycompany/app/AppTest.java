d6b80a889ccc7d8aab597c32759ab8fe4339ad95
Shreyansh Raut <79033673+ShreyanshRaut@users.noreply.github.com>
2024-07-28 16:40:41 -0400

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello, I am Shreyansh!!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello, I am Shreyansh!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
